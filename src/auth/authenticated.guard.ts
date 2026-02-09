8a053afb139c50817d3bcf6bbaabf19f1f9586e2
Gentahall <gentahalilintar36@gmail.com>
2026-02-09 11:08:32 +0700

    update: button logout & expired session

--- src/auth/authenticated.guard.ts
+++ src/auth/authenticated.guard.ts
@@ -5,12 +5,16 @@ import { Response } from 'express';
 export class AuthenticatedGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    const response = context.switchToHttp().getResponse<Response>();
+    const response = context.switchToHttp().getResponse<Response>(); 
 
     const user = (request.session as any).user;
 
     if (user) {
-      return true; 
+      response.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+      response.header('Expires', '-1');
+      response.header('Pragma', 'no-cache');
+      
+      return true;
     }
 
     response.redirect('/auth/login');
